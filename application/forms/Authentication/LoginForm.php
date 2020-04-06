6f242bff39c10adbc8c234ea8d656e9bc0a13efc
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-04-06 15:23:03 +0200

    emove unnecessary brackets

--- application/forms/Authentication/LoginForm.php
+++ application/forms/Authentication/LoginForm.php
@@ -113,7 +113,7 @@ class LoginForm extends Form
             if ($this->getElement('rememberme')->isChecked()) {
                     $rememberMe = RememberMe::fromCredentials($user->getUsername(), $password);
                     $this->getResponse()->setCookie(
-                        ($rememberMe->getCookie())
+                        $rememberMe->getCookie()
                     );
 
                     $rememberMe->persist();

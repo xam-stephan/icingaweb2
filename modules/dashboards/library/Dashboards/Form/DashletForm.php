e98c18f19d1842881a60047fc53c59b91101f6b0
Yonas Habteab <yonas.habteab@icinga.com>
2020-05-19 13:25:44 +0200

    Update mysql insert statement

--- modules/dashboards/library/Dashboards/Form/DashletForm.php
+++ modules/dashboards/library/Dashboards/Form/DashletForm.php
@@ -30,65 +30,31 @@ class DashletForm extends DashboardsForm
 
     protected function onSuccess()
     {
-        if ($this->getValue('user-dashboard') !== null) {
-            if ($this->getValue('new-dashboard-name') !== null) {
-                $data = [
-                    'dashboard_id' => $this->createDashboard($this->getValue('new-dashboard-name')),
-                    'user_name' => Auth::getInstance()->getUser()->getUsername()
-                ];
+        if ($this->getValue('new-dashboard-name') !== null) {
+            $this->getDb()->insert('dashlet', [
+                'dashboard_id'  => $this->createDashboard($this->getValue('new-dashboard-name')),
+                'name'          => $this->getValue('name'),
+                'url'           => $this->getValue('url'),
+                'type'          => $this->getValue('dashboard-type'),
+                'owner'         => $this->getValue('dashboard-type') === 'private'?
+                    Auth::getInstance()->getUser()->getUsername(): null
+            ]);
 
-                $this->getDb()->insert('user_dashboard', $data);
-
-                $this->getDb()->insert('user_dashlet', [
-                    'dashlet_id' => $this->createUserDashlet($this->getValue('new-dashboard-name')),
-                    'user_dashboard_id' => $this->fetchUserDashboardId($this->getValue('new-dashboard-name'))
-                ]);
-
-                Notification::success("Private dashboard and dashlet created");
-            } else {
-                if (! $this->checkForPrivateDashboard($this->getValue('dashboard'))) {
-                    $this->getDb()->insert('user_dashboard', [
-                        'dashboard_id' => $this->getValue('dashboard'),
-                        'user_name' => Auth::getInstance()->getUser()->getUsername()
-                    ]);
-
-                    $this->getDb()->insert('user_dashlet', [
-                        'dashlet_id' => $this->createUserDashlet($this->getValue('dashboard')),
-                        'user_dashboard_id' => $this->fetchUserDashboardId($this->getValue('dashboard'))
-                    ]);
-
-                    Notification::success("Private dashlet in a public dashboard created!");
-                } else {
-                    $this->getDb()->insert('user_dashlet', [
-                        'dashlet_id' => $this->createUserDashlet($this->getValue('dashboard')),
-                        'user_dashboard_id' => $this->fetchUserDashboardId($this->getValue('dashboard'))
-                    ]);
-
-                    Notification::success("Private dashlet created");
-                }
-            }
-        } elseif ($this->checkForPrivateDashboard($this->getValue('dashboard'))) {
-            Notification::error("You can't create public dashlet in a private dashboard!");
-        } elseif (Auth::getInstance()->getUser()->isMemberOf('admin')) {
-            if ($this->getValue('new-dashboard-name') !== null) {
-                $this->getDb()->insert('dashlet', [
-                    'dashboard_id' => $this->createDashboard($this->getValue('new-dashboard-name')),
-                    'name' => $this->getValue('name'),
-                    'url' => $this->getValue('url')
-                ]);
-
-                Notification::success('Dashboard and dashlet created');
-            } else {
-                $this->getDb()->insert('dashlet', [
-                    'dashboard_id' => $this->getValue('dashboard'),
-                    'name' => $this->getValue('name'),
-                    'url' => $this->getValue('url'),
-                ]);
-
-                Notification::success('Dashlet created');
-            }
+            Notification::success("Dashboard and dashlet created");
+        } elseif ($this->checkForPrivateDashboard($this->getValue('dashboard')) &&
+            $this->getValue('dashboard-type') === 'system') {
+            Notification::error("Public dashlets in a private dashboard are not allowed!");
         } else {
-            Notification::error("You don't have a permission!");
+            $this->getDb()->insert('dashlet', [
+                'dashboard_id'  => $this->getValue('dashboard'),
+                'name'          => $this->getValue('name'),
+                'url'           => $this->getValue('url'),
+                'type'          => $this->getValue('dashboard-type'),
+                'owner'         => $this->getValue('dashboard-type') === 'private'?
+                    Auth::getInstance()->getUser()->getUsername(): null
+            ]);
+
+            Notification::success("Dashlet created!");
         }
     }
 }

5ae39af4fe00139eca5d99078b4b134d3f73cfd0
Yonas Habteab <yonas.habteab@icinga.com>
2020-05-25 16:00:21 +0200

    Add additional tables for rearranging dashboards and dashlets

--- modules/dashboards/etc/schema/mysql.schema.sql
+++ modules/dashboards/etc/schema/mysql.schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS dashboard_order;
+DROP TABLE IF EXISTS dashlet_order;
+DROP TABLE IF EXISTS dashboard_user_order;
+DROP TABLE IF EXISTS dashlet_user_order;
 DROP TABLE IF EXISTS  dashlet;
 DROP TABLE IF EXISTS dashboard;
 
@@ -16,4 +20,30 @@ CREATE TABLE dashlet (
     type enum('system', 'private') NOT NULL,
     owner varchar(254) DEFAULT NULL COLLATE utf8mb4_unicode_ci,
     CONSTRAINT fk_dashlet_dashboard FOREIGN KEY (dashboard_id) REFERENCES dashboard (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE dashboard_order (
+    dashboard_id int(10) unsigned NOT NULL,
+    `order` tinyint unsigned NOT NULL,
+    CONSTRAINT fk_dashboard_order_dashboard FOREIGN KEY (dashboard_id) REFERENCES dashboard (id) ON DELETE CASCADE ON UPDATE CASCADE
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE Table dashlet_order (
+    dashlet_id int(10) unsigned NOT NULL,
+    `order` tinyint unsigned NOT NULL,
+    CONSTRAINT fk_dashlet_order_dashlet FOREIGN KEY (dashlet_id) REFERENCES dashlet (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE dashboard_user_order (
+    dashboard_id int(10) unsigned NOT NULL,
+    username varchar(254) NOT NULL COLLATE utf8mb4_unicode_ci,
+    `order` tinyint unsigned NOT NULL,
+    CONSTRAINT fk_user_dashboard_order_dashboard FOREIGN KEY (dashboard_id) REFERENCES dashboard(id) ON DELETE CASCADE ON UPDATE CASCADE
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE dashlet_user_order (
+    dashlet_id int(10) unsigned NOT NULL,
+    username varchar(254) NOT NULL COLLATE utf8mb4_unicode_ci,
+    `order` tinyint unsigned NOT NULL,
+    CONSTRAINT fk_user_dashlet_order_dashlet FOREIGN KEY (dashlet_id) REFERENCES dashlet (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;

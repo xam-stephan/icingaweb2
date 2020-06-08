3c54ed31962f6c6b5f5b972d822361ea6dbf579a
Yonas Habteab <yonas.habteab@icinga.com>
2020-06-08 16:41:40 +0200

    Add mysql table comments

--- modules/dashboards/etc/schema/mysql.schema.sql
+++ modules/dashboards/etc/schema/mysql.schema.sql
@@ -15,12 +15,12 @@ CREATE TABLE dashboard_home (
 
 CREATE TABLE dashboard (
     id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    home_id int(10) unsigned NOT NULL COMMENT 'Dashboards to be shared',
+    home_id int(10) unsigned NOT NULL,
     name varchar(64) NOT NULL COLLATE utf8mb4_unicode_ci,
     owner varchar(254) DEFAULT NULL COLLATE utf8mb4_unicode_ci,
     shared enum('enforced', 'n', 'y') NOT NULL,
     CONSTRAINT fk_dashboard_dashboard_home FOREIGN KEY (home_id) REFERENCES dashboard_home (id) ON DELETE CASCADE ON UPDATE CASCADE
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT 'Dashboards to be shared';
 
 CREATE TABLE dashlet (
     id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -33,33 +33,33 @@ CREATE TABLE dashlet (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE dashboard_user (
-    dashboard_id int(10) unsigned COMMENT 'Dashboards enforced for certain users' NOT NULL,
+    dashboard_id int(10) unsigned NOT NULL,
     user varchar(254) NOT NULL COLLATE utf8mb4_unicode_ci,
     CONSTRAINT fk_user_dashboard_dashboard FOREIGN KEY (dashboard_id) REFERENCES dashboard (id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT 'Dashboards enforced for certain users';
 
 CREATE TABLE dashboard_home_order (
-    home varchar(64) NOT NULL COMMENT 'Order of dashboard homes per user' COLLATE utf8mb4_unicode_ci,
+    home varchar(64) NOT NULL COMMENT 'varchar instead of id because of system dashboards' COLLATE utf8mb4_unicode_ci,
     user varchar(254) DEFAULT NULL COLLATE utf8mb4_unicode_ci,
     `order` tinyint unsigned NOT NULL
-) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT 'Order of dashboard homes per user';
 
 CREATE TABLE dashboard_order (
-    home varchar(64) NOT NULL COMMENT 'Order of dashboards per home per user' COLLATE utf8mb4_unicode_ci,
+    home varchar(64) NOT NULL COLLATE utf8mb4_unicode_ci,
     dashboard varchar(64) NOT NULL COLLATE utf8mb4_unicode_ci,
     user varchar(254) DEFAULT NULL COLLATE utf8mb4_unicode_ci,
     `order` tinyint unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT 'Order of dashboards per home per user';
 
 CREATE Table dashlet_order (
-    dashlet VARCHAR(64) NOT NULL COMMENT 'Order of dashlets per home & per user' COLLATE utf8mb4_unicode_ci,
+    dashlet VARCHAR(64) NOT NULL COLLATE utf8mb4_unicode_ci,
     user varchar(254) DEFAULT NULL COLLATE utf8mb4_unicode_ci,
     `order` tinyint unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE dashboard_share(
-    dashboard_id int(10) unsigned NOT NULL COMMENT 'Shares taken per user' COLLATE utf8mb4_unicode_ci,
+    dashboard_id int(10) unsigned NOT NULL COLLATE utf8mb4_unicode_ci,
     user varchar(254) NOT NULL COLLATE utf8mb4_unicode_ci,
     home varchar(64) NOT NULL COLLATE utf8mb4_unicode_ci,
     CONSTRAINT fk_share_dashboard_dashboard FOREIGN KEY (dashboard_id) REFERENCES dashboard (id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT 'Shares taken per user';

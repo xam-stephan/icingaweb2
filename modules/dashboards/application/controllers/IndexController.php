47a77dc9b76323e58df04759b030d50f448b5372
Yonas Habteab <yonas.habteab@icinga.com>
2020-06-08 16:22:07 +0200

    Add union statement to the mysql query

--- modules/dashboards/application/controllers/IndexController.php
+++ modules/dashboards/application/controllers/IndexController.php
@@ -5,9 +5,9 @@ namespace Icinga\Module\Dashboards\Controllers;
 use Icinga\Authentication\Auth;
 use Icinga\Module\Dashboards\Common\Database;
 use Icinga\Module\Dashboards\Web\Controller;
-use Icinga\Module\Dashboards\Web\Widget\CreateDefaultDashlets;
 use Icinga\Module\Dashboards\Web\Widget\Tabextension\DashboardAction;
 use Icinga\Module\Dashboards\Web\Widget\DashboardWidget;
+use Icinga\Web\Notification;
 use Icinga\Web\Url;
 use ipl\Sql\Select;
 
@@ -20,30 +20,17 @@ class IndexController extends Controller
         try {
             $this->createTabsAndAutoActivateDashboard();
         } catch (\Exception $e) {
-            CreateDefaultDashlets::createAction();
             $this->createTabsAndAutoActivateDashboard();
+
+            Notification::error("No dashboard or dashlet found!");
         }
 
-        $query = (new Select())
-            ->columns('*')
-            ->from('dashboard')
-            ->where(['id = ?' => $this->tabs->getActiveName(), 'type = ?' => 'system']);
-
-        $dashboard = $this->getDb()->select($query)->fetch();
-
-        //Note: In private dashboards type, the dashlets are sorted by user_dashlets_order
-        // and in system dashboards the private dashlets are always sorted according to the public dashlets
-        //TODO: If you want to have the dashlets the other way around, you just have to adjust the function call orderBy
         $select = (new Select())
             ->columns('dashlet.*')
-            ->joinLeft('dashlet_order do', 'do.dashlet_id = dashlet.id')
-            ->joinLeft('dashlet_user_order duo', 'duo.dashlet_id = dashlet.id')
             ->from('dashlet')
             ->where([
                 'dashlet.dashboard_id = ?' => $this->tabs->getActiveName(),
-                'duo.username IS NULL OR duo.username = ?' => Auth::getInstance()->getUser()->getUsername()
-            ])
-            ->orderBy($dashboard ? 'do.order' : 'duo.order', 'DESC');
+            ]);
 
         $dashlets = $this->getDb()->select($select);
 
@@ -63,15 +50,23 @@ class IndexController extends Controller
         $tabs = $this->getTabs();
 
         $select = (new Select())
-            ->columns('dashboard.*, do.`order`')
-            ->from('dashboard')
-            ->joinLeft('dashboard_order do', 'do.dashboard_id = dashboard.id')
-            ->joinLeft('dashboard_user_order duo', 'duo.dashboard_id = dashboard.id')
-            ->where([
-                'dashboard.type = "system" OR dashboard.owner = ?' => Auth::getInstance()->getUser()->getUsername(),
-                'duo.username IS NULL OR duo.username = ?' => Auth::getInstance()->getUser()->getUsername()
-            ])
-            ->orderBy('do.`order`, dashboard.id');
+            ->columns('dashboard_home.*')
+            ->from('dashboard_home')
+            ->where(['dashboard_home.owner = ?' => Auth::getInstance()->getUser()->getUsername()]);
+
+        $query = (new Select())
+            ->columns('dashboard_home.*')
+            ->from('dashboard_home')
+            ->join('dashboard', 'dashboard.home_id = dashboard_home')
+            ->join('dashboard_user', 'dashboard_user.dashboard_id = dashboard.id')
+            ->where(['dashboard_user.user = ?' => Auth::getInstance()->getUser()->getUsername()]);
+
+        $query->unionAll($select)
+            ->joinLeft(
+                'dashboard_home_order dho',
+                ['dho.home = dashboard_home.name AND dho.user = ?' => Auth::getInstance()->getUser()->getUsername()])
+            ->groupBy('dashboard_home.name, dho.`order`')
+            ->orderBy('dho.`order`, dashboard_home.name');
 
         $dashboards = $this->getDb()->select($select);
 
